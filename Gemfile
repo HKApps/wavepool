9609d6e9b420fcaf572fb9dfb49212eae0876906
Matt Nguyen <mnguyen916@gmail.com>
2013-06-18 19:34:55 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0.rc2'
+gem 'jbuilder', '~> 1.2'
+gem 'pg'
+gem 'turbolinks'
+gem 'unicorn'
+
+group :assets do
+  gem 'coffee-rails', '~> 4.0.0'
+  gem 'jquery-rails'
+  gem 'sass-rails', '~> 4.0.0.rc2'
+  gem 'uglifier', '>= 1.3.0'
+end
+
+group :doc do
+  gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Whirlwind::Application.config.secret_key_base = 'd48fe8346fdf377798366802f6d03b7b06009bb21bc9d6192c4326e8a3c69b068e3f38117d3560ef0c95d6f7c06539b920bced880bdebafc4ce3dda97958f2b6'
