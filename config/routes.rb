a5b1d56cc47811cbfa29221abbf9de022ff82eea
Matt Nguyen <mnguyen916@gmail.com>
2013-06-30 17:08:28 -0700

    Auth with rdio

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Whirlwind::Application.routes.draw do
+  root to: 'sessions#new'
+
+  get '/login', to: "sessions#new"
+
+  match '/auth/rdio/callback', to: 'sessions#create', via: [:get, :post]
+  match '/logout', to: 'sessions#destroy', via: [:get, :post]
+
+  resources :playlists
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   #root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
