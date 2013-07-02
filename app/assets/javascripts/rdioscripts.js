8e9fd38bdd70d6e2369fa2267f99bbe33e0a5427
Basil Siddiqui <basil.siddiqui@gmail.com>
2013-07-01 23:49:29 -0400

    finished basic interface and rdio playback

--- app/assets/javascripts/rdioscripts.js
+++ app/assets/javascripts/rdioscripts.js
@@ -1,34 +1,47 @@
 var duration = 1; // track the duration of the currently playing track
-    $(document).ready(function() {
-      $('#rdio').bind('ready.rdio', function() {
-        $(this).rdio().play('a1228601');
-      });
-      $('#rdio').bind('playingTrackChanged.rdio', function(e, playingTrack, sourcePosition) {
-        if (playingTrack) {
-          duration = playingTrack.duration;
-          $('#art').attr('src', playingTrack.icon);
-          $('#track').text(playingTrack.name);
-          $('#artist').text(playingTrack.artist);
-        }
-        });
-      $('#rdio').bind('positionChanged.rdio', function(e, position) {
-        $('#position').css('width', Math.floor(100*position/duration)+'%');
-      });
-      $('#rdio').bind('playStateChanged.rdio', function(e, playState) {
-        if (playState == 0) { // paused
-          $('#play').show();
-          $('#pause').hide();
-        } else {
-          $('#play').hide();
-          $('#pause').show();
-        }
-      });
-      // this is a valid playback token for localhost.
-      // but you should go get your own for your own domain.
-      $('#rdio').rdio('GAlNi78J_____zlyYWs5ZG02N2pkaHlhcWsyOWJtYjkyN2xvY2FsaG9zdEbwl7EHvbylWSWFWYMZwfc=');
+var playstate = 2; // start with player stopped
+$(document).ready(function() {
 
-      $('#previous').click(function() { $('#rdio').rdio().previous(); });
-      $('#play').click(function() { $('#rdio').rdio().play(); });
-      $('#pause').click(function() { $('#rdio').rdio().pause(); });
-      $('#next').click(function() { $('#rdio').rdio().next(); });
-    });
+  $('#rdio').bind('ready.rdio', function() {
+  });
+
+  // update the track views when tracks change
+  $('#rdio').bind('playingTrackChanged.rdio', function(e, playingTrack, sourcePosition) {
+    if (playingTrack) {
+      duration = playingTrack.duration;
+      $('#art').attr('src', playingTrack.icon);
+      $('#track').text(playingTrack.name);
+      $('#artist').text(playingTrack.artist);
+    }
+  });
+
+  $('#rdio').bind('positionChanged.rdio', function(e, position) {
+    $('#position').css('width', Math.floor(100*position/duration)+'%');
+  });
+
+  $('#rdio').bind('playStateChanged.rdio', function(e, playState) {
+    playstate = playState;
+    // 0 → paused, 1 → playing, 2 → stopped, 3 → buffering, 4 → paused.
+  });
+
+  // this is a valid playback token for localhost
+  $('#rdio').rdio('GAlNi78J_____zlyYWs5ZG02N2pkaHlhcWsyOWJtYjkyN2xvY2FsaG9zdEbwl7EHvbylWSWFWYMZwfc=');
+
+  // playlist controls
+  $('#play').click(function() {
+    if (playstate == 2) { // stopped
+      setPlaylist();
+    }
+    else {
+      $('#rdio').rdio().play();
+    }
+  });
+  $('#pause').click(function() { $('#rdio').rdio().pause(); });
+  $('#previous').click(function() { $('#rdio').rdio().previous(); });
+  $('#next').click(function() { $('#rdio').rdio().next(); });
+});
+
+function setPlaylist(){
+  playlist = $('#playlist_key').html(); // pretending this is response
+  $('#rdio').rdio().play(playlist);
+}
