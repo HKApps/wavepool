bc7f461a12e80311a0b71c4f94168e989c12e034
Basil Siddiqui <basil.siddiqui@gmail.com>
2013-07-07 17:01:54 -0400

    fixed rdio playing

--- app/assets/javascripts/rdioscripts.js
+++ app/assets/javascripts/rdioscripts.js
@@ -1,35 +1,37 @@
 var duration = 1; // track the duration of the currently playing track
 var playstate = 2; // start with player stopped
-R.ready(function() {
+$(document).ready(function(){
+  console.log("ready");
+  R.ready(function() {
+    // update the track views when tracks change
+    R.player.on("change:playingTrack", function(playingTrack) {
+      if (playingTrack) {
+        p = playingTrack;
+        duration = playingTrack.attributes.duration;
+        $('#art').attr('src', playingTrack.attributes.icon);
+        $('#track').text(playingTrack.attributes.name);
+        $('#artist').text(playingTrack.attributes.artist);
+      }
+    });
 
-  // update the track views when tracks change
-  R.player.on("change:playingTrack", function(playingTrack) {
-    if (playingTrack) {
-      p = playingTrack;
-      duration = playingTrack.attributes.duration;
-      $('#art').attr('src', playingTrack.attributes.icon);
-      $('#track').text(playingTrack.attributes.name);
-      $('#artist').text(playingTrack.attributes.artist);
-    }
-  });
+    R.player.on("change:position", function(position) {
+      // $('#position').css('width', Math.floor(100*position/duration)+'%');
+    });
 
-  R.player.on("change:position", function(position) {
-    // $('#position').css('width', Math.floor(100*position/duration)+'%');
-  });
+    R.player.on("change:playState", function(state) {
+    });
 
-  R.player.on("change:playState", function(state) {
+    // playlist controls
+    $('#start').click(function() {
+      setPlaylist();
+      $('#start').css("display", "none");
+      $('#rdioPlayer').css("display", "");
+    });
+    $('#play').click(function() { R.player.play(); });
+    $('#pause').click(function() { R.player.pause(); });
+    $('#previous').click(function() { R.player.previous(); });
+    $('#next').click(function() { R.player.next(); });
   });
-
-  // playlist controls
-  $('#start').click(function() {
-    setPlaylist();
-    $('#start').css("display", "none");
-    $('#rdioPlayer').css("display", "");
-  });
-  $('#play').click(function() { R.player.play(); });
-  $('#pause').click(function() { R.player.pause(); });
-  $('#previous').click(function() { R.player.previous(); });
-  $('#next').click(function() { R.player.next(); });
 });
 
 function setPlaylist(){
