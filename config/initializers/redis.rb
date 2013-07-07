1fea3c29dd5e010c15ca2517d0e3c4dfe3425c92
Matt Nguyen <mnguyen916@gmail.com>
2013-07-07 14:50:30 -0700

    set up prod redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,8 @@
 require 'redis'
 
-$redis = Redis.new(:host => 'localhost', :port => 6379)
+$redis = if Rails.env.production? || Rails.env.staging?
+          uri = URI.parse(ENV["REDISTOGO_URL"])
+          Redis.new(host: uri.host, port: uri.port, password: uri.password)
+        else
+          Redis.new
+        end
