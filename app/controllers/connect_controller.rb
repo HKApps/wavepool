aa3a182fc968a9daec522815cccdfe4485ad8165
Matt Nguyen <mnguyen916@gmail.com>
2013-07-09 21:34:22 -0700

    Change default to song request in connect

--- app/controllers/connect_controller.rb
+++ app/controllers/connect_controller.rb
@@ -7,17 +7,19 @@ class ConnectController < ApplicationController
         r.Sms "Thanks! What song would like to request?\n\nFormat your request like '[title] - [artist]'.\n\nFor example: Get Lucky - Daft Punk."
       end
       render xml: twiml_response.text
-    elsif sms_receiver.song_request?
-      params.merge!(rdio_key: sms_receiver.playlist.rdio_key)
-      redirect_to song_search_path(params)
     elsif sms_receiver.song_decision?
       params.merge!(rdio_key: sms_receiver.playlist.rdio_key)
       redirect_to add_song_to_playlist_path(params)
     else
+     if sms_receiver.song_request?
+      params.merge!(rdio_key: sms_receiver.playlist.rdio_key)
+     else
+      redirect_to song_search_path(params)
       twiml_response = Twilio::TwiML::Response.new do |r|
         r.Sms "Sorry! We couldn't process your request! :("
       end
       render xml: twiml_response.text
+     end
     end
   end
 end
